d06db17b2c998d535ae620f4a4e2755f87770d29
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-01-12 16:44:00 +1300

    Linked original stylesheet to main.js

--- lung-app/src/main.js
+++ lung-app/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '/Users/alyssalaterveer/Documents/ABI/VueProjects/LungWebAppVisualisations/css/style.css'
 
 Vue.config.productionTip = false
 
