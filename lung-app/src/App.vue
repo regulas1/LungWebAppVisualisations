0fd7983eeef2eb22a587566f69a4aa2557f4bb2e
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-01-14 12:01:59 +1300

    Added navigation tree image as a module

--- lung-app/src/App.vue
+++ lung-app/src/App.vue
@@ -2,16 +2,25 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--Display navigation tree-->
+    <img alt="Navigation tree" :src="fullTree" height="500px"/>  <!--Need to bind since using image as module-->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import nav from './assets/fullTree.png' /* Fetch fullTree image from assets */
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  /* data must be a function: https://vuejs.org/v2/guide/components.html */
+  data: function() { 
+    return {
+      fullTree: nav /* https://stackoverflow.com/questions/45116796/how-to-import-and-use-image-in-a-vue-single-file-component */ 
+    }
   }
 }
 </script>
