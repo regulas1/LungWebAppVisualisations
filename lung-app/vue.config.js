8c2764e3366f9ad8d1668270c653ec83e9843b41
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-01-14 16:51:33 +1300

    Added Gas Exchange page

--- lung-app/vue.config.js
+++ lung-app/vue.config.js
@@ -22,5 +22,20 @@ module.exports = {
       // and falls back to `public/index.html` if not found.
       // Output filename is inferred to be `subpage.html`.
       /* subpage: 'src/subpage/main.js' */
+      gasExchange: { /* Can use any name */
+        // entry for the page
+        entry: 'src/pages/Breathing/GasExchange/main.js', /* Must use correct pathname (ie correct folder name capitalisation) */
+        // the source template
+        template: 'public/breathing-gas-exchange.html',
+        /* Appends "index.html" onto parent directory ("public"), 
+        must use page with HTML header (not a section id) */
+        filename: 'index.html',
+        // when using title option,
+        // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+        title: 'Breathing Gas Exchange Page',
+        // chunks to include on this page, by default includes
+        // extracted common chunks and vendor chunks.
+        /* chunks: ['chunk-vendors', 'chunk-common', 'index'] */
+      },
     }
   }
