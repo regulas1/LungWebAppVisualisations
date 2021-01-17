e34d1f0f60a121ba548b3a85d1fc6819854d0861
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-01-18 10:19:52 +1300

    implemented HelloCube using MSD approach

--- blog-free-v1.0.0/src/main.js
+++ blog-free-v1.0.0/src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+/*
 import {
   Camera,
   Group,
@@ -12,64 +13,23 @@ import {
 } from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';*/
 
-import {
-  BoxBufferGeometry,
-  Color,
-  Mesh,
-  MeshBasicMaterial,
-  PerspectiveCamera,
-  WebGLRenderer,
-} from 'three';
+import { World } from './World/World.js';
 
-// Get a reference to the container element that will hold our scene
-const container = document.querySelector('#scene-container');
+function main() {
+  // Get a reference to the container element
+  const container = document.querySelector('#scene-container');
 
-// create a Scene
-const scene = new Scene();
+  // 1. Create an instance of the World app
+  const world = new World(container);
 
-// Set the background color
-scene.background = new Color('skyblue');
+  // 2. Render the scene
+  world.render();
+}
 
-// Create a camera
-const fov = 35; // angle that camera perspective spans out at
-const aspect = container.clientWidth / container.clientHeight;
-const near = 0.1; // distance behind camera to near clipping plane
-const far = 100; // distance behind camera to far clipping plane
+main();
 
-const camera = new PerspectiveCamera(fov, aspect, near, far);
-
-// every object is initially created at ( 0, 0, 0 )
-// move the camera back so we can view the scene
-camera.position.set(0, 0, 10);
-
-// create a geometry
-const geometry = new BoxBufferGeometry(2, 2, 2);
-
-// create a default (white) Basic material
-const material = new MeshBasicMaterial();
-
-// create a Mesh containing the geometry and material
-const cube = new Mesh(geometry, material);
-
-// add the mesh to the scene
-scene.add(cube);
-
-// create the renderer
-const renderer = new WebGLRenderer();
-
-// next, set the renderer to the same size as our container element
-renderer.setSize(container.clientWidth, container.clientHeight);
-
-// finally, set the pixel ratio so that our scene will look good on HiDPI displays
-renderer.setPixelRatio(window.devicePixelRatio);
-
-// add the automatically created <canvas> element to the page
-container.append(renderer.domElement);
-
-// render, or 'create a still image', of the scene
-renderer.render(scene, camera);
 
 Vue.config.productionTip = false
 
