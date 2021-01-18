63eb9d4e938e76a81074939075d00cb00c0b2ad2
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-01-18 17:13:25 +1300

    Updated HelloCube to include Physically Based Rendering & Lighting

--- blog-free-v1.0.0/src/World/World.js
+++ blog-free-v1.0.0/src/World/World.js
@@ -1,5 +1,6 @@
 import { createCamera } from './components/camera.js';
 import { createCube } from './components/cube.js';
+import { createLights } from './components/lights.js';
 import { createScene } from './components/scene.js';
 
 import { createRenderer } from './systems/renderer.js';
@@ -7,6 +8,7 @@ import { Resizer } from './systems/Resizer.js';
 
 // These variables are module-scoped: we cannot access them
 // from outside the module
+// (using this.camera = createCamera would allow it to be accessed from main.js, which we do NOT want)
 let camera;
 let renderer;
 let scene;
@@ -18,15 +20,23 @@ class World {
     renderer = createRenderer();
     container.append(renderer.domElement);
 
+    // Cube does not need to be a module scope variable since it is only used in the constructor
     const cube = createCube();
 
-    scene.add(cube);
+    const light = createLights();
+
+    scene.add(cube, light); // Add cube & light components simultaneously
 
     const resizer = new Resizer(container, camera, renderer);
+    // .onResize calls World.render
+    resizer.onResize = () => {
+      this.render();
+    };
   }
 
   render() {
     // draw a single frame
+    // i.e. no animation loop
     renderer.render(scene, camera);
   }
 }
