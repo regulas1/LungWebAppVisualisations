8e9f06ae2d06160f2b746768a66ab55fd7931916
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-01-21 14:08:40 +1300

    Imprved main.js notation

--- lung-app/src/main.js
+++ lung-app/src/main.js
@@ -1,8 +1,24 @@
+/**************************************************************************
+	* IMPORTS
+	***************************************************************************/
+
+// NPM: MAIN
 import Vue from "vue";
+
+// PROJECT: MAIN
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+
+// PROJECT: OTHER
+import "@/components/js/scene";
+
+
+
+/**************************************************************************
+	* VUE INSTANCE
+	 ***************************************************************************/
 
 Vue.config.productionTip = false;
 
