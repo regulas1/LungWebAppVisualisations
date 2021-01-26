4928b682c864a6960872ffd125134e38747fa457
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-01-26 17:16:31 +1300

    Set up an enabledItem variable to track which buttons have been clicked

--- lung-app/src/components/base/NavBtn.vue
+++ lung-app/src/components/base/NavBtn.vue
@@ -2,18 +2,19 @@
     <div> <!--Need extra div since we must have a single root component-->
         <radial-menu
             style="margin: auto; margin-top: 150px; background-color: white"
-            :itemSize=50
+            :size="70"
+            :itemSize="80"
             :radius="120"
-            :angle-restriction="180">
+            :angle-restriction="100">
             <radial-menu-item 
                 v-for="(item, index) in items" 
                 :key="index" 
                 style="background-color: white" 
                 @click="() => handleClick(item)">
-            <span>{{item}}</span>
+            <span align=center>{{item}}</span>
             </radial-menu-item>
         </radial-menu>
-        <div style="color: rgba(0,0,0,0.6); margin-top: 16px;">{{ lastClicked }}</div>
+        <div style="color: rgba(0,0,0,0.6); margin-top: 16px;">{{ lastClicked }} {{ enabledItem}} </div>
     </div>
 </template>
 
@@ -28,14 +29,17 @@ export default {
   },
   data () {
     return {
-      items: ['Breathing', 'Asthma', 'Smoking', 'Disease',],
-      lastClicked: 'click on something!'
+      items: ['Learn More', 'Pause Model', 'My Info',],
+      lastClicked: 'click on something!',
+      enabledItem: [false, false, false], // Boolean to represent current state of button
     }
   },
   methods: {
     handleClick (item) {
       this.lastClicked = item;
+      this.enabledItem = !this.enabledItem; // Change boolean value to opposite of current button state
     }
   }
 }
+
 </script>
