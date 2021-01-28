216d6e7ca70f5f325e092e6058c93b8801335dff
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-01-28 15:31:48 +1300

    Created page navigation option buttons

--- lung-app/src/views/Breathing.vue
+++ lung-app/src/views/Breathing.vue
@@ -5,8 +5,59 @@
 	************************************************************************ -->  
 
 <template>
+
+<div>
+    <v-container>
+        <v-col
+            v-for="(item, i) in items"
+            :key="i"
+            cols="12"
+            md="3"
+          >
+            <v-card
+              :color="item.color"
+              dark
+            >
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <div>
+                  <v-card-title
+                    class="headline"
+                    v-text="item.title"
+                  ></v-card-title>
+  
+                  <v-card-subtitle v-text="item.description"></v-card-subtitle>
+  
+                  <v-card-actions>
+                    <v-btn
+                      v-if="item.description === 'Click to play the video'"
+                      class="ml-2 mt-3"
+                      fab
+                      icon
+                      height="40px"
+                      right
+                      width="40px"
+                    >
+                      <v-icon>mdi-play</v-icon>
+                    </v-btn>
+  
+                    <v-btn
+                      v-else
+                      class="ml-2 mt-5"
+                      outlined
+                      rounded
+                      small
+                    >
+                      LEARN MORE
+                    </v-btn>
+                  </v-card-actions>
+                </div>
+              </div>
+            </v-card>
+          </v-col>
+          </v-container>
+
     <v-container class="grey lighten-5">
-        <v-row>
+        <v-row class="yellow lighten-3 fill-height">
             <v-col
                 cols="12"
                 md="3"
@@ -97,6 +148,7 @@
         <!--</v-row>-->
         <subscribe />
     </v-container>
+</div>
 </template>
 
 <!-- *************************************************************************
@@ -109,6 +161,22 @@
 
     export default {
         name: 'Breathing',
+        data: () => ({
+            items: [
+                {
+                color: '#385F73',
+                //src: './path/to/url',
+                title: 'The Lungs',
+                description: 'How breathing works',
+            },
+            {
+                color: '#1F7087',
+                //src: './path/to/url',
+                title: 'Gas Exchange',
+                description: 'Why do we need it?',
+            },
+            ],
+        }),
         components: {
             Subscribe: () => import('@/components/home/Subscribe'),
             NavBtn: () => import('@/components/base/NavBtn'),
