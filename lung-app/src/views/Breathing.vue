467812314c806214774943bdefd8af9551e3c6f3
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-01-29 14:35:43 +1300

    Added new cards to breathing page

--- lung-app/src/views/Breathing.vue
+++ lung-app/src/views/Breathing.vue
@@ -1,167 +1,140 @@
 <!-- *************************************************************************
 	TEMPLATE
-	************************************************************************ -->  
-
+	************************************************************************ -->
 <template>
-<div>
-    <v-container class="grey lighten-5">
-        <v-row class="grey lighten-3 fill-height">
-            <v-col
-                cols="12"
-                md="3"
-            > 
-            <v-row>
-            <base-test-card>
-                <template v-slot:header>
-                    The Lungs
-                </template>
+    <v-container>
+        <base-subheading>Breathing</base-subheading> <!--Main heading-->
+        <v-container class="grey lighten-5">
+            <v-row class="grey lighten-3 fill-height">
+                <v-col
+                    cols="12"
+                    md="3"
+                > 
+                <!-- ------------------------------------------------------------------------
+                                                TOP LEFT CONTAINER
+                    ------------------------------------------------------------------------- -->
+                    <v-row>
+                        <base-reveal>
+                            <template v-slot:header>
+                                Normal Breathing
+                            </template>
 
-                <template v-slot:description>
-                    <p>Have you ever wondered why you seem to feel breathless?</p>
-                    <p>Click "Learn More" to display information about how the lungs actually work.</p>
-                </template>
+                            <template v-slot:subheader>
+                                What our lungs should do
+                            </template>
 
-                <template v-slot:hiddenHeader>
-                    Normal Breathing
-                </template>
+                            <template v-slot:description>
+                                <p>
+                                    When the healthy lung breathes in the upright position, the lung tissue that is closest to the 
+                                    diaphragm expands the most. The tissue at the top of 
+                                    the lung expands the least.
+                                </p>
+                                <p>
+                                    Any activity we do requires a certain amount of energy. We usually breathe in
+                                    just enough air to supply oxygen needed to create that energy.
+                                </p>
+                            </template>
+                        </base-reveal>
+                    </v-row>
 
-                <template v-slot:hiddenDescription>
-                    <p>
-                        When the healthy lung breathes in the upright position, the lung tissue that is closest to the 
-                        diaphragm expands the most. The tissue at the top of 
-                        the lung expands the least.
-                    </p>
-                    <p>
-                        We usually breathe in just as much air as we need to supply oxygen for 
-                        energy for the activities that we are doing.
-                    </p>
-                </template>
-            </base-test-card>
-            </v-row>
+                <!-- ------------------------------------------------------------------------
+                                            BOTTOM LEFT CONTAINER
+                    ------------------------------------------------------------------------- -->
+                    <v-row>
+                        <base-reveal>
+                            <template v-slot:header>
+                                Healthy Gas Exchange
+                            </template>
+
+                            <template v-slot:subheader>
+                                Oxygen IN and carbon dioxide OUT
+                            </template>
+
+                            <template v-slot:description>
+                                <p>
+                                    Air (oxygen &amp; carbon dioxide) is transported to/from our lungs via 30,000 airways. Blood is carried to the lungs via blood vessels.
+                                    Deep within the lungs, there are gas exchange units called <strong>alveoli</strong>.  
+                                </p>
+                                <p>    
+                                    Air and blood are delivered to the alveoli. Oxygen mixes with blood to form oxygenated blood, which is carried to the heart and delivered around the body. 
+                                    Carbon dioxide must be removed from the blood and exhaled.
+                                </p>
+                                <p>
+                                    Gas exchange works best when the same amount of air and blood is delivered to the alveoli. 
+                                    Lung diseases affect the amount of air or blood that is delivered: some get too much, and some get too little.
+                                </p>
+                            </template>
+                        </base-reveal>
+                    </v-row>
+                </v-col>
+
             <!-- ------------------------------------------------------------------------
-                                        TOP LEFT CONTAINER
+                                            CENTRE CONTAINER
                 ------------------------------------------------------------------------- -->
-                <v-row> 
+                 <v-col
+                    cols="6"
+                >Second column: lung model goes here
+                </v-col>
+
+            <!-- ------------------------------------------------------------------------
+                                            TOP RIGHT CONTAINER
+                ------------------------------------------------------------------------- -->
+                <v-col
+                    cols="6"
+                    md="3"
+                > 
                     <base-options 
-                    color = "#385F73"
-                    button = "Learn more"
-                    >
-                    <template v-slot:header>
-                        The Lungs
-                    </template>
-
-                    <template v-slot:description>
-                        <p>
-                            <br>
-                            <strong>Normal Breathing</strong>
-                            <br>
-                        </p>
-                        <p>
-                            When the healthy lung breathes in the upright position, the lung tissue that is closest to the 
-                            diaphragm expands the most. The tissue at the top of 
-                            the lung expands the least.
-                        </p>
-                        <p>
-                            We usually breathe in just as much air as we need to supply oxygen for 
-                            energy for the activities that we are doing.
-                        </p>
-                    </template>
-                    </base-options>
-                </v-row>
-
-            <!-- ------------------------------------------------------------------------
-                                        BOTTOM LEFT CONTAINER
-                ------------------------------------------------------------------------- -->
-                <v-row> 
-                    <base-options 
-                    color = "#1F7087"
-                    button = "Learn more"
-                    >
-                    <template v-slot:header>
-                        Gas Exchange
-                    </template>
-
-                    <template v-slot:description>
-                        <p>
-                            <br>
-                            <strong>Why do we need it?</strong>
-                            <br>
-                        </p>
-                        <p>
-                            Paragraph text
-                        </p>
-                        <p>
-                            Paragraph text
-                        </p>
-                    </template>
-                    </base-options>
-                </v-row>
-            </v-col>
-
-            <!-- ------------------------------------------------------------------------
-                                        CENTRE CONTAINER
-                ------------------------------------------------------------------------- -->
-            <v-col
-                cols="6"
-            >Second column: lung model goes here
-            </v-col>
-
-            <!-- ------------------------------------------------------------------------
-                                        TOP RIGHT CONTAINER
-                ------------------------------------------------------------------------- -->
-            <v-col
-                cols="6"
-                md="3"
-            > 
-                <base-options 
-                    color = "grey"
-                    button = "none" 
+                        color = "grey"
+                        button = "none" 
                     > <!--Since button option text is "none", no button will be displayed-->
-                    <template v-slot:header>
-                        Key Vocabulary
-                    </template>
+                        <template v-slot:header>
+                            Key Vocabulary
+                        </template>
 
-                    <template v-slot:description>
-                        <p>
-                            <br>
-                            <strong>Diaphragm:</strong>
-                            <br>
-                            The muscle that sits beneath the lungs.
-                        </p>
-                        <p>
-                            <br>
-                            <strong>Expands:</strong>
-                            <br>
-                            Gets bigger by filling with air (similar to a balloon).
-                        </p>
-                    </template>
-                </base-options>
-            </v-col>
-        </v-row>
-        <nav-btn />
+                        <template v-slot:description>
+                            <p>
+                                <br>
+                                <strong>Alveoli</strong>
+                                <br>
+                                Site/unit of gas exchange at the end of airways deep within the lungs
+                            </p>
+                            <p>
+                                <br>
+                                <strong>Diaphragm</strong>
+                                <br>
+                                The muscle that sits beneath the lungs.
+                            </p>
+                            <p>
+                                <br>
+                                <strong>Expands</strong>
+                                <br>
+                                Gets bigger by filling with air (similar to a balloon).
+                            </p>
+                        </template>
+                    </base-options>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-container>
-</div>
 </template>
 
 <!-- *************************************************************************
 	SCRIPT
-	************************************************************************ -->  
+	************************************************************************ -->
 <script>
-    //import NavBtn from '@/components/base/NavBtn'
 import BaseOptions from '@/components/base/PageOptions'
-import BaseTestCard from '@/components/base/TestCard'
+import BaseReveal from '@/components/base/RevealCard'
 
-    export default {
-        name: 'Breathing',
-        components: {
-            NavBtn: () => import('@/components/base/NavBtn'),
-            BaseOptions,
-            BaseTestCard,
-            //BaseLayout // Undefined component (since BaseLayout is a global plugin)
-        }
+export default {
+    name: 'Disease', /* Change name of component */ 
+    components: {
+        BaseOptions,
+        BaseReveal,
     }
+};
 </script>
 
 <!-- *************************************************************************
 	STYLE
-	************************************************************************ -->  
+	************************************************************************ -->
+<style scoped></style>
