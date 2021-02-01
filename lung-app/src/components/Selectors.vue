875bb517a924ef56bde4b218fbbc0fa2dfc4e52f
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-02-01 22:57:18 +1300

    Button selectors for number of packs smoked added, still a couple bugs to fix

--- lung-app/src/components/Selectors.vue
+++ lung-app/src/components/Selectors.vue
@@ -2,6 +2,7 @@
 	TEMPLATE
 	************************************************************************ -->
 <template>
+<div>
     <v-toolbar
         prominent
         rounded
@@ -13,6 +14,8 @@
         <base-gender-select />
         <base-FEV1-select />
     </v-toolbar>
+    <base-pack-select />
+</div>
 </template>
 
 <!-- *************************************************************************
@@ -22,6 +25,7 @@
 import BaseAgeSelect from '@/components/base/AgeSelect'
 import BaseGenderSelect from '@/components/base/GenderSelect'
 import BaseFEV1Select from '@/components/base/FEV1Select'
+import BasePackSelect from '@/components/base/PackSelect'
 
 export default {
     name: "Selectors",
@@ -29,6 +33,7 @@ export default {
         BaseAgeSelect,
         BaseGenderSelect,
         BaseFEV1Select,
+        BasePackSelect,
     }
 }
 </script>
---
+++ lung-app/src/components/base/PackSelect.vue
@@ -0,0 +1,58 @@
+<!-- *************************************************************************
+	TEMPLATE
+	************************************************************************ -->
+<template>
+    <v-btn-toggle
+            v-model="toggle_exclusive"
+            active-class="white--text"
+          >
+            <v-btn 
+              color="blue"
+              class="ma-1"
+              outlined
+             >
+              Heal Me
+            </v-btn>
+            <v-btn 
+              active-class="green white--text"
+              class="ma-1"
+              outlined
+             >
+              Zero
+            </v-btn>
+            <v-btn 
+              active-class="orange white--text"
+              class="ma-1"
+              outlined
+             >
+              1 Pack
+            </v-btn>
+            <v-btn 
+              active-class="red white--text"
+              class="ma-1"
+              outlined
+             >
+              2 Packs
+            </v-btn>
+          </v-btn-toggle>
+</template>
+
+<!-- *************************************************************************
+	SCRIPT
+	************************************************************************ -->
+<script>
+export default {
+    name: 'BasePackSelect', /* Change name of component */ 
+    data: function() {
+        return{
+                toggle_exclusive: 2,
+        }
+    },
+};
+</script>
+
+<!-- *************************************************************************
+	STYLE
+	************************************************************************ -->
+<style scoped>
+</style>
