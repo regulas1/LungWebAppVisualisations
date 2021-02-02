0527a2605d43a5e08ea9a23ec044295d72a16cff
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-02-02 14:35:35 +1300

    Installed Vue-Typical to add animation to the homepage title, but it is not yet working

--- lung-app/src/components/home/Banner.vue
+++ lung-app/src/components/home/Banner.vue
@@ -21,6 +21,16 @@
           <div class="subheading text-uppercase pl-2 mb-4">
             Use your mouse to rotate the interactive lung model
           </div>
+          <typical 
+        :steps="['Hello', 1000, 'Hello world!', 500]" 
+        :wrapper="'h2'"
+      ></typical>
+      <typical 
+        class="typicalWrapper"
+        :steps="['So', 1000, 'So Awesome!', 500, 'So Awesome! Aha :-) 👋', 1000]" 
+        :loop=3 
+        :wrapper="'h3'"
+      ></typical>
 
           <v-btn
             color="primary"
@@ -34,7 +44,10 @@
 </template>
 
 <script>
+import typical from '@/components/home/Typical.vue'
+
   export default {
     name: 'HomeBanner',
+    components: typical
   }
 </script>
