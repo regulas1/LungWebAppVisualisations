a6fd4313e9b87ff17ad61bdf8ffee53be1f2584e
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-02-12 08:07:41 +1300

    Got example model into lung-app: https://github.com/anthonysimone/threejs-vue-example

--- lung-app/src/views/Home.vue
+++ lung-app/src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="home">
-    <!--Main images on Homepage-->
-    <!--<articles>
-      <banner />
-    </articles>-->
     <banner />
-
-    <model-json />
-
+  
+    <div class="model-preview">
+      <model-gltf />
+      hello
+      <!--<model-json />-->
+    </div>
+    
     <subscribe />
 
    
@@ -15,7 +15,8 @@
 </template>
 
 <script>
-import ModelJson from '@/components/ModelJSON.vue';
+import ModelGltf from '@/components/ModelGLTF.vue'
+//import ModelJson from '@/components/ModelJSON.vue';
 
   export default {
     name: 'Home',
@@ -24,7 +25,15 @@ import ModelJson from '@/components/ModelJSON.vue';
       //Articles: () => import('@/components/home/Articles'), // Corresponds to dimensions of main image on Homepage
       Banner: () => import('@/components/home/Banner'),
       Subscribe: () => import('@/components/home/Subscribe'),
-      ModelJson,
+      //ModelJson,
+      ModelGltf,
     },
   }
 </script>
+
+<style scoped>
+.model-preview {
+  width: "260px";
+  height: "260px";
+}
+</style>
