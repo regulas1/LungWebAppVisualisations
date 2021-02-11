3918d5c5483f5438191a135dfa996dd33989d9fb
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-02-12 11:25:32 +1300

    Tried to change size to not full window

--- lung-app/src/components/ModelGLTF.vue
+++ lung-app/src/components/ModelGLTF.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="scene-container" ref="sceneContainer">
-    Hello
+    <canvas />
   </div>
 </template>
 
@@ -28,18 +28,18 @@ export default {
       this.container = this.$refs.sceneContainer
       // add stats
       this.stats = new Stats()
-      this.container.appendChild(this.stats.dom)
+      this.container.appendChild(this.stats.dom) // Displays  progress stats box
       // add camera
       const fov = 45 // Field of view
-      const aspect = this.container.clientWidth / this.container.clientHeight
-      const near = 0.1 // the near clipping plane
+      const aspect = 2 // this.container.clientWidth / this.container.clientHeight
+      const near = 0.01 // the near clipping plane
       const far = 1000 // the far clipping plane
       const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
       camera.position.set(0, 5, 10)
       this.camera = camera
       // create scene
       this.scene = new THREE.Scene()
-      this.scene.background = new THREE.Color('darkgrey')
+      this.scene.background = new THREE.Color('skyblue')
       // add lights
       const ambientLight = new THREE.HemisphereLight(
         0xffffff, // bright sky color
@@ -52,17 +52,17 @@ export default {
       // add controls
       this.controls = new OrbitControls(this.camera, this.container)
       // create renderer
-      this.renderer = new THREE.WebGLRenderer({ antialias: true })
-      this.renderer.setSize(this.container.clientWidth, this.container.clientHeight)
+      this.renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("canvas") })
+      /*this.renderer.setSize(this.container.clientWidth, this.container.clientHeight)
       this.renderer.setPixelRatio(window.devicePixelRatio)
       this.renderer.gammaFactor = 2.2
       this.renderer.outputEncoding = THREE.sRGBEncoding
       this.renderer.physicallyCorrectLights = true
-      this.container.appendChild(this.renderer.domElement)
+      this.container.appendChild(this.renderer.domElement)*/
       // set aspect ratio to match the new browser window aspect ratio
-      this.camera.aspect = this.container.clientWidth / this.container.clientHeight
-      this.camera.updateProjectionMatrix()
-      this.renderer.setSize(this.container.clientWidth, this.container.clientHeight)
+      //this.camera.aspect = this.container.clientWidth / this.container.clientHeight
+      //this.camera.updateProjectionMatrix()
+      //this.renderer.setSize(this.container.clientWidth, this.container.clientHeight)
       const loader = new GLTFLoader()
       loader.load(
         '/three-assets/RobotExpressive.glb',
@@ -76,9 +76,23 @@ export default {
         this.render()
       })
     },
+    resizeCanvasToDisplaySize () {
+      const canvas = this.renderer.domElement;
+      const width = this.canvas.clientWidth;
+      const height = this.canvas.clientHeight;
+
+      if (canvas.width !== width || canvas.height !== height) {
+        // Mustust pass false here or three.js will fight browser
+        this.renderer.setSize(width, height, false);
+        this.camera.aspect = width / height;
+        this.camera.updateProjectionMatrix();
+      }
+    },
     render () {
+      this.resizeCanvasToDisplaySize()
       this.renderer.render(this.scene, this.camera)
       this.stats.update()
+
     }
   },
   mounted () {
