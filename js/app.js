4b62bf1f0a925def69fdf6d9e0cd4111f2d151f7
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-02-23 10:47:53 +1300

    Added vuetify & navigation buttons (not yet working)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,38 @@
+const Breathing = { template: '<div>Breathing</div>' }
+const Asthma = { template: '<div>Asthma</div>' }
+
+  const routes = [{
+    path: '/',
+    component: Breathing
+},
+{
+    path: '/breathing',
+    component: Breathing
+},
+{
+    path: '/asthma',
+    component: Asthma
+},
+]
+
+let router = new VueRouter({
+    routes // short for `routes: routes`
+})
+
+const vuetifyOptions = { }
+Vue.use(Vuetify)
+
+
 const app = new Vue({
-    el: '#app-one',
+    el: '#app',
+    watch: {},
+    mounted() {
+
+    },
     data: {
-        title: 'Vue App'
-    }
+        title: 'Lungs',
+    },
+    methods: {},
+    router,
+    vuetify: new Vuetify(vuetifyOptions),
 })
