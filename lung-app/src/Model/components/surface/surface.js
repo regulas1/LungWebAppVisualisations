6eb95aeb98591ef07d1309f18f801f49e0cb02f8
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-02-25 18:09:01 +1300

    Tried using stringify first, still same error

--- lung-app/src/Model/components/surface/surface.js
+++ lung-app/src/Model/components/surface/surface.js
@@ -1,10 +1,23 @@
-import { FileLoader, ObjectLoader } from 'zincjs/node_modules/three/src/loaders/FileLoader.js'; 
+import { FileLoader, ObjectLoader } from 'zincjs/node_modules/three'; 
 
 // Get setupModel module
 import { setupModel } from './setupModel.js';
 
+String.prototype.hashCode = function() {
+	var hash = 0, i, chr;
+	if (this.length === 0) return hash;
+	for (i = 0; i < this.length; i++) {
+		chr   = this.charCodeAt(i);
+		hash  = ((hash << 5) - hash) + chr;
+		hash |= 0; // Convert to 32bit integer
+	}
+	return hash;
+};
+
 async function loadSurface() {
   const loader = new FileLoader();
+
+  //let data = JSON.stringify('/three-assets/Lung/surface_1.json').hashCode();
 
   const surfaceData = await loader.loadAsync(
     '/three-assets/Lung/surface_1.json',
