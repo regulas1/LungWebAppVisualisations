ba99f73cc04decb385ba4f77c51583e744eb6c34
Alyssa Laterveer <alat076@aucklanduni.ac.nz>
2021-02-25 23:07:50 +1300

    Fixed the parse error, but object is undefined

--- lung-app/src/Model/components/surface/surface.js
+++ lung-app/src/Model/components/surface/surface.js
@@ -3,28 +3,17 @@ import { FileLoader, ObjectLoader } from 'zincjs/node_modules/three';
 // Get setupModel module
 import { setupModel } from './setupModel.js';
 
-String.prototype.hashCode = function() {
-	var hash = 0, i, chr;
-	if (this.length === 0) return hash;
-	for (i = 0; i < this.length; i++) {
-		chr   = this.charCodeAt(i);
-		hash  = ((hash << 5) - hash) + chr;
-		hash |= 0; // Convert to 32bit integer
-	}
-	return hash;
-};
-
 async function loadSurface() {
   const loader = new FileLoader();
 
-  //let data = JSON.stringify('/three-assets/Lung/surface_1.json').hashCode();
-
   const surfaceData = await loader.loadAsync(
     '/three-assets/Lung/surface_1.json',
-    function (text) { 
-      let json = JSON.parse(text);
-      let object = (new ObjectLoader()).parse(json, 'path');
-      object.geometry.morphColors = json.morphColors;
+    function(text) {
+      console.log('This is the text: ', text)
+      //let json = JSON.parse(data);
+      //console.log(json)
+      //let object = (new ObjectLoader().parse());
+      //console.log(object)
     },
     function (err) {
       console.error('Could not load model: ', err);
