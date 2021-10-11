745c8681dfbc0f4800b33177a74217d2306a882f
sriregula <sriregula@outlook.com>
2021-10-12 10:57:40 +1300

    Added the show lungs button

---
+++ src/components/base/BtnShowLungs.vue
@@ -0,0 +1,44 @@
+<!-- *************************************************************************
+	TEMPLATE
+	************************************************************************ -->
+<template>
+  <div>
+    <div>
+      <button v-on:click="isHidden = !isHidden">Hide show Lungs</button>
+
+
+         <v-col
+                    cols="12"
+                    md="6"
+                    align="center"
+                    v-if="!isHidden"
+                >
+        <GltfModel> </GltfModel>
+      
+    </div>
+  </div>
+</template>
+
+<!-- *************************************************************************
+	SCRIPT
+	************************************************************************ -->
+<script>
+import GltfModel from "@/components/ModelGLTFthree";
+export default {
+  name: "Modal",
+  data() {
+    return {
+      isHidden: false
+    };
+  },
+
+  components: {
+    GltfModel
+  }
+};
+</script>
+
+<!-- *************************************************************************
+	STYLE
+	************************************************************************ -->
+<style scoped></style>
